f266cb98ac3608baee1b0400ef443a9583693b74
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2022-11-17 12:34:55 +0530

    Added New Project

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,6 +5,7 @@ import project_1 from "./screencapture-portfolio-portfolio-01-netlify-app-2022-1
 import project_2 from "./screencapture-portfolio-saas-01-netlify-app-2022-11-10-17_28_53.png";
 import project_3 from "./screencapture-portfolio-landingpage-01-netlify-app-2022-11-10-17_28_25.png";
 import project_4 from "./screencapture-localhost-3000-profile-2022-11-15-12_33_51.png";
+import project_5 from "./Portfolio-Project-Temp.png";
 
 export {
   logo,
@@ -14,4 +15,5 @@ export {
   project_2,
   project_3,
   project_4,
+  project_5,
 };
